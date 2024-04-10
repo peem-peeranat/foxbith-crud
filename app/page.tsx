6b9a48bd2b85f4615b7b05d90d237fab75022395
Peem Peeranat <peempeeranat@gmail.com>
2024-04-10 15:59:03 +0700

    first-commit'

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,112 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// import styles from "./page.module.css";
+import {
+  Box,
+  Typography,
+  Button,
+  TextField,
+  Container,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormControl,
+  FormLabel
+} from "@mui/material";
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      {/* --------------------------------headder-------------------------------------- */}
+      <Box sx={{ backgroundColor: 'white' }}>
+        <Box sx={{ py: 3, px: 3, borderBottom: '1px solid #dddddd' }}>
+          <Typography variant="h5">
+            🦊 Foxbith Questionnaire
+          </Typography>
+        </Box>
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end', py: '12px', px: 3, gap: '12px', borderBottom: '1px solid #dddddd' }}>
+          <Button variant="outlined" sx={{ borderColor: '#FF5C00', py: '13px', px: 2 }}>
+            <Typography sx={{ color: '#FF5C00' }}>
+              Cancle
+            </Typography>
+          </Button>
+          <Button variant="contained" sx={{ borderColor: '#FF5C00', backgroundColor: '#FF5C00', Color: 'White', py: '13px', px: 9 }}>
+            <Typography>
+              Save
+            </Typography>
+          </Button>
+        </Box>
+      </Box>
+      {/* --------------------------------headder-------------------------------------- */}
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <Box sx={{ m: 3, borderRadius: '10px', backgroundColor: 'white', boxShadow: '50px' }}>
+        <Box>
+          <Box sx={{ py: 3, px: 3, borderBottom: '1px solid #dddddd' }}>
+            <Typography variant="h6" sx={{ mb: 3 }}>Questionnaire Detail</Typography>
+            <TextField id="outlined-basic" label="Name" variant="outlined" sx={{ width: '100%' }} />
+          </Box>
+        </Box>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+        {/* --------------------------------Questionnaire-------------------------------------- */}
+        <Box sx={{ px: 3, py: 3 }}>
+          <Box sx={{ display: "flex", flexDirection: 'column', gap: 3 }}>
+            <Typography variant="h6">Question 1</Typography>
+            <TextField id="outlined-basic" label="Question" variant="outlined" sx={{ width: '100%' }} />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+            <RadioGroup name="use-radio-group" sx={{ gap: 3 }}>
+              <Box sx={{ display: 'flex', alignItems: 'center', gap: 3 }}>
+                <FormControlLabel value="female" control={<Radio />} label="" />
+                <TextField id="outlined-basic" label="Description *" variant="outlined" sx={{ width: '100%' }} />
+                <Button sx={{ color: 'black', px: 0 }}>
+                  <DeleteOutlineIcon />
+                </Button>
+              </Box>
+              <Box sx={{ display: 'flex', alignItems: 'center', gap: 3 }}>
+                <FormControlLabel value="male" control={<Radio />} label="" />
+                <TextField id="outlined-basic" label="Description *" variant="outlined" sx={{ width: '100%' }} />
+                <Button sx={{ color: 'black', px: 0 }}>
+                  <DeleteOutlineIcon />
+                </Button>
+              </Box>
+            </RadioGroup>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+            <Box sx={{
+              borderBottom: '1px solid #dddddd',
+              pb: 3
+            }}>
+              <Button sx={{ borderColor: '#FF5C00', backgroundColor: 'tranparent', Color: 'White' }}>
+                <Typography sx={{ color: '#FF5C00' }}>
+                  + ADD CHOICE
+                </Typography>
+              </Button>
+            </Box>
+            <Box sx={{ display: 'flex' }}>
+              <Button sx={{ color: 'black' }}>
+                <Box sx={{ display: 'flex', pr: 3, gap: 1 }}>
+                  <ContentCopyIcon />
+                  <Typography >
+                    Duplicate
+                  </Typography>
+                </Box>
+              </Button>
+              <Button sx={{ color: 'black' }}>
+                <Box sx={{ display: 'flex', pr: 3, gap: 1 }}>
+                  <DeleteOutlineIcon />
+                  <Typography >
+                    Delete
+                  </Typography>
+                </Box>
+              </Button>
+            </Box>
+          </Box >
+        </Box>
+        {/* --------------------------------Questionnaire-------------------------------------- */}
+        <Button sx={{ borderColor: '#FF5C00', backgroundColor: 'tranparent', Color: 'White', width: '100%', py: '13px', border: '1px solid #FF5C00' }}>
+          <Typography sx={{ color: '#FF5C00' }}>
+            + ADD QUESTION
+          </Typography>
+        </Button>
+      </Box>
+    </>
+  )
 }
