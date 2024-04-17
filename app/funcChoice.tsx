f2430cd820cc6ef5af9bd6f35ee3abdfe463d46b
Peem Peeranat <130559407+peem147peem@users.noreply.github.com>
2024-04-17 18:30:21 +0700

    'add-func-question-add-duplicate-delete'

--- app/funcChoice.tsx
+++ app/funcChoice.tsx
@@ -5,8 +5,18 @@ export interface Choice {
   value: string;
   description: string;
 }
+export interface Question {
+  id: number;
+  value: string;
+  description: string;
+}
 
 export const funcChoice = () => {
+
+  const [questions, setQuestions] = useState<Question[]>([
+    { id: 1, value: '', description: '' },
+  ]);
+
   const [choices, setChoices] = useState<Choice[]>([
     { id: 0, value: '', description: '' },
     { id: 1, value: '', description: '' },
@@ -20,14 +30,46 @@ export const funcChoice = () => {
     );
   };
 
+
+  const handleQuestionChange = (id: number, value: string) => {
+    setQuestions(prevQuestions =>
+      prevQuestions.map(question =>
+        question.id === id ? { ...question, description: value } : question
+      )
+    );
+  };
+
+
   const addChoice = () => {
     const newChoice = { id: choices.length + 1, value: '', description: '' };
     setChoices([...choices, newChoice]);
   };
 
+  const addQuestion = () => {
+    const newQuestion = { id: questions.length + 1, value: '', description: '' };
+    setQuestions([...questions, newQuestion]);
+  };
+
+
   const removeChoice = (id: number) => {
     setChoices(prevChoices => prevChoices.filter(choice => choice.id !== id));
   };
 
-  return { choices, handleChoiceChange, addChoice, removeChoice };
+
+  const removeQuestion = (id: number) => {
+    setQuestions(prevQuestions => {
+      const updatedQuestions = prevQuestions.filter(question => question.id !== id);
+      return updatedQuestions.map((question, index) => ({ ...question, id: index + 1 }));
+    });
+  };
+
+  const duplicateQuestion = (id: number) => {
+    const questionDuplicate = questions.find(question => question.id === id);
+    if (questionDuplicate) {
+      const newQuestion = { ...questionDuplicate, id: questions.length + 1 };
+      setQuestions(prevQuestions => [...prevQuestions, newQuestion]);
+    }
+  };
+
+  return { choices, questions, handleChoiceChange, handleQuestionChange, addChoice, removeChoice, addQuestion, removeQuestion, duplicateQuestion };
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import React from 'react';
+
 import {
   Box,
   Typography,
@@ -15,7 +16,9 @@ import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import { funcChoice } from "./funcChoice";
 
 export default function Home() {
-  const { choices, handleChoiceChange, addChoice, removeChoice } = funcChoice();
+  const { choices, questions, handleChoiceChange, handleQuestionChange, addChoice, removeChoice, addQuestion, removeQuestion, duplicateQuestion } = funcChoice();
+
+
   return (
     <>
       {/* --------------------------------headder-------------------------------------- */}
@@ -31,7 +34,7 @@ export default function Home() {
               Cancle
             </Typography>
           </Button>
-          <Button variant="contained" sx={{ borderColor: '#FF5C00', backgroundColor: '#FF5C00', Color: 'White', py: '13px', px: 9 }}>
+          <Button variant="contained" sx={{ rderColor: '#FF5C00', backgroundColor: '#FF5C00', Color: 'White', py: '13px', px: 9 }}>
             <Typography>
               Save
             </Typography>
@@ -43,64 +46,79 @@ export default function Home() {
         <Box>
           <Box sx={{ py: 3, px: 3, borderBottom: '1px solid #dddddd' }}>
             <Typography variant="h6" sx={{ mb: 3 }}>Questionnaire Detail</Typography>
-            <TextField id="outlined-basic" label="Name" variant="outlined" sx={{ width: '100%' }} />
+            <TextField
+              required
+              id="outlined-basic"
+              label="Name"
+              variant="outlined"
+              sx={{ width: '100%' }} />
           </Box>
         </Box>
         {/* --------------------------------Question-------------------------------------- */}
         <Box sx={{ px: 3, py: 3 }}>
-          <Box sx={{ display: "flex", flexDirection: 'column', gap: 3 }}>
-            <Typography variant="h6">Question 1</Typography>
-            <TextField id="outlined-basic" label="Question" variant="outlined" sx={{ width: '100%' }} />
-            <RadioGroup name="use-radio-group" sx={{ gap: 3 }}>
-              {choices.map(choice => (
-                <Box sx={{ display: 'flex', alignItems: 'center', gap: 3 }}>
-                  <FormControlLabel value={choice.id} control={<Radio />} label="" />
-                  <TextField
-                    required
-                    id="outlined-basic"
-                    label="Description"
-                    variant="outlined"
-                    value={choice.description}
-                    onChange={(e) => handleChoiceChange(choice.id, e.target.value)}
-                    sx={{ width: '100%' }} />
-                  <Button sx={{ color: 'black', px: 0 }} onClick={() => removeChoice(choice.id)}>
+          {questions.map(question => (
+            <Box sx={{ display: "flex", flexDirection: 'column', gap: 3 }}>
+              <Typography variant="h6">Question {question.id}</Typography>
+              <TextField
+                required
+                id="outlined-basic"
+                label="Question"
+                variant="outlined"
+                sx={{ width: '100%' }}
+                value={question.description}
+                onChange={(e) => handleQuestionChange(question.id, e.target.value)}
+              />
+              <RadioGroup name="use-radio-group" sx={{ gap: 3 }}>
+                {choices.map(choice => (
+                  <Box sx={{ display: 'flex', alignItems: 'center', gap: 3 }}>
+                    <FormControlLabel value={choice.id} control={<Radio />} label="" />
+                    <TextField
+                      required
+                      id="outlined-basic"
+                      label="Description"
+                      variant="outlined"
+                      value={choice.description}
+                      onChange={(e) => handleChoiceChange(choice.id, e.target.value)}
+                      sx={{ width: '100%' }} />
+                    <Button sx={{ color: 'black', px: 0 }} onClick={() => removeChoice(choice.id)}>
+                      <DeleteOutlineIcon />
+                    </Button>
+                  </Box>
+                ))}
+              </RadioGroup>
+              <Box sx={{
+                borderBottom: '1px solid #dddddd',
+                pb: 3
+              }}>
+                <Button sx={{ borderColor: '#FF5C00', backgroundColor: 'tranparent', Color: 'White' }} onClick={addChoice}>
+                  <Typography sx={{ color: '#FF5C00' }}>
+                    + ADD CHOICE
+                  </Typography>
+                </Button>
+              </Box>
+              <Box sx={{ display: 'flex' }}>
+                <Button sx={{ color: 'black' }} onClick={() => duplicateQuestion(question.id)}>
+                  <Box sx={{ display: 'flex', pr: 3, gap: 1 }}>
+                    <ContentCopyIcon />
+                    <Typography >
+                      Duplicate
+                    </Typography>
+                  </Box>
+                </Button>
+                <Button sx={{ color: 'black' }} onClick={() => removeQuestion(question.id)}>
+                  <Box sx={{ display: 'flex', pr: 3, gap: 1 }}>
                     <DeleteOutlineIcon />
-                  </Button>
-                </Box>
-              ))}
-            </RadioGroup>
-            <Box sx={{
-              borderBottom: '1px solid #dddddd',
-              pb: 3
-            }}>
-              <Button sx={{ borderColor: '#FF5C00', backgroundColor: 'tranparent', Color: 'White' }} onClick={addChoice}>
-                <Typography sx={{ color: '#FF5C00' }}>
-                  + ADD CHOICE
-                </Typography>
-              </Button>
-            </Box>
-            <Box sx={{ display: 'flex' }}>
-              <Button sx={{ color: 'black' }}>
-                <Box sx={{ display: 'flex', pr: 3, gap: 1 }}>
-                  <ContentCopyIcon />
-                  <Typography >
-                    Duplicate
-                  </Typography>
-                </Box>
-              </Button>
-              <Button sx={{ color: 'black' }}>
-                <Box sx={{ display: 'flex', pr: 3, gap: 1 }}>
-                  <DeleteOutlineIcon />
-                  <Typography >
-                    Delete
-                  </Typography>
-                </Box>
-              </Button>
-            </Box>
-          </Box >
+                    <Typography >
+                      Delete
+                    </Typography>
+                  </Box>
+                </Button>
+              </Box>
+            </Box >
+          ))}
         </Box>
         {/* --------------------------------Add-QUESTION-------------------------------------- */}
-        <Button sx={{ borderColor: '#FF5C00', backgroundColor: 'tranparent', Color: 'White', width: '100%', py: '13px', border: '1px solid #FF5C00' }}>
+        <Button sx={{ borderColor: '#FF5C00', backgroundColor: 'tranparent', Color: 'White', width: '100%', py: '13px', border: '1px solid #FF5C00' }} onClick={addQuestion}>
           <Typography sx={{ color: '#FF5C00' }}>
             + ADD QUESTION
           </Typography>
